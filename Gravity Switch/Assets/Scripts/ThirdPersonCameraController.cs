6819b615e80f60e012c7195bf77a4f5a2058b850
mythguy1226 <robbiereddick@aol.com>
2021-11-09 21:22:25 -0500

    Implemented Player Movement and Camera Controls

---
+++ Gravity Switch/Assets/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    // Fields needed for transforming the camera
+    public float rotationSpeed = 1;
+    public Transform target;
+    public Transform player;
+    float mouseX;
+    float mouseY;
+
+    // Update Method 
+    void Update()
+    {
+        CameraControl();
+    }
+
+    // Method that controls Camera Movement and Rotation
+    void CameraControl()
+    {
+        // Get the Inputs and clamp the Y value to only go a certain range
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        // Update the Camera to look at the target
+        transform.LookAt(target);
+
+        // Update the rotations of both the camera target and the player as the mouse moves
+        target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        player.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+}
---
+++ Gravity Switch/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    // Fields needed for player functionality
+    public float speed = 20.0f;
+    Rigidbody playerBody;
+    public float jumpSpeed = 40.0f;
+    bool canJump;
+
+    // Init
+    private void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+        canJump = true;
+    }
+    // Update Method
+    void Update()
+    {
+        // Call Player Updates
+        PlayerMovement();
+    }
+
+    // Method that Updates Player position based on input
+    void PlayerMovement()
+    {
+        // Get the Input for moving the character
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+
+        // Store user input as a movement vector and then move the player
+        Vector3 m_Input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        playerBody.MovePosition(transform.position + transform.TransformDirection(m_Input) * Time.deltaTime * speed);
+
+        // Implementation for Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            playerBody.AddForce(new Vector3(0, jumpSpeed, 0), ForceMode.Impulse);
+            canJump = false;
+        }
+    }
+
+    // Method for Collision Resolution
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Platform collisions
+        if (collision.gameObject.name.Contains("Platform"))
+        {
+            // Reset capability to jump
+            canJump = true;
+        }
+    }
+}
