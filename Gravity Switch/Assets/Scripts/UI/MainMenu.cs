537233a0852e93ccef64018adc473af48f5fbe01
Kimmy Goewey <kpg6728@rit.edu>
2021-11-14 20:06:41 -0500

    added very basic main menu

---
+++ Gravity Switch/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Play() {
+        SceneManager.LoadScene(1);
+    }
+    public void Quit() {
+        Application.Quit(); 
+    }
+}
